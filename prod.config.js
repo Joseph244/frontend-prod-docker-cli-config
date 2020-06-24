32c6cc27e2e112182ccbc9b9c1f98baf2a1d127e
zhangzhengfei <zhengfeizhang@sina.com>
2020-06-24 11:20:46 +0800

    fix

--- prod.config.js
+++ prod.config.js
@@ -8,11 +8,11 @@ module.exports = {
     name: "prod106环境",
     host: "192.168.78.106", // 服务器地址,其中xxx为手动输入ip末尾
     port: 22, // ssh port，一般默认22
-    username: "root", // 登录服务器用户名
-    password: "xxxxxx", // 登录服务器密码
+    username: "", // 登录服务器用户名
+    password: "", // 登录服务器密码
     distPath: "dist", // 当前工程中需要上传服务器的文件夹（前不加斜杠标识相对当前工程目录，如加斜杠就需要写出本地完整绝对路径)
     filter: "*", // 过滤distPath内的文件（只考虑单层文件，不支持递归），只有符合正则的才会上传
-    webDir: "/root/temp/messageRouteApp", // 打包文件上传服务器地址(会将distPath文件夹放过来，如果是nginx做前端配置的话要加distPath)
+    webDir: "/root/temp/messageRouteApp", // 打包文件上传服务器地址(会将上一行配置的distPath文件夹上传到该位置)
     dockerName: "messageroute", // docker名称
     dockerWebDir: "/usr/app", // 容器中文件存放位置
   },
@@ -20,11 +20,11 @@ module.exports = {
     name: "conf106环境",
     host: "192.168.78.106", // 服务器地址,其中xxx为手动输入ip末尾
     port: 22, // ssh port，一般默认22
-    username: "root", // 登录服务器用户名
-    password: "xxxxxx", // 登录服务器密码
+    username: "", // 登录服务器用户名
+    password: "", // 登录服务器密码
     distPath: "vue.config.js", // 当前工程中需要上传服务器的文件名
     filter: "*", // 过滤distPath内的文件（只考虑单层文件，不支持递归），只有符合正则的才会上传
-    webDir: "/root/temp/messageRouteApp", // 打包文件上传服务器地址(会将distPath文件夹放过来，如果是nginx做前端配置的话要加distPath)
+    webDir: "/root/temp/messageRouteApp", // 打包文件上传服务器地址
     // dockerName: 'messageroute', // docker名称
     // dockerWebDir: '/usr/app' // 容器中文件存放位置
   },
@@ -33,8 +33,8 @@ module.exports = {
     name: "104环境",
     host: "192.168.78.104", // 服务器地址,其中xxx为手动输入ip末尾
     port: 22, // ssh port，一般默认22
-    username: "root", // 登录服务器用户名
-    password: "xxxxxx", // 登录服务器密码
+    username: "", // 登录服务器用户名
+    password: "", // 登录服务器密码
     distPath: "dist", // 需要上传服务器的文件夹（前不加斜杠标识相对当前工程目录，如加斜杠就是完整绝对路径)
     filter: "*", // 过滤distPath内的文件（只考虑单层文件，不支持递归），只有符合正则的才会上传
     webDir: "/opt/nginx/",
@@ -43,11 +43,11 @@ module.exports = {
     name: "228环境",
     host: "192.168.78.228", // 服务器地址,其中xxx为手动输入ip末尾
     port: 22, // ssh port，一般默认22
-    username: "root", // 登录服务器用户名
-    password: "xxxxxx", // 登录服务器密码
+    username: "", // 登录服务器用户名
+    password: "", // 登录服务器密码
     distPath: "dist", // 需要上传服务器的文件夹（前不加斜杠标识相对当前工程目录，如加斜杠就是完整绝对路径)
     filter: "*", // 过滤distPath内的文件（只考虑单层文件，不支持递归），只有符合正则的才会上传
-    webDir: "/root/temp/messageRouteApp",
+    webDir: "/root/temp/messageRouteApp/dist",
     dockerName: "messageroute", // docker名称
     dockerWebDir: "/usr/app", // 容器中文件存放位置
   },
