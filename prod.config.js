f65c681442a454be2ecc5618d59397f1dff9a027
zhangzhengfei <zhengfeizhang@sina.com>
2020-06-24 10:26:36 +0800

    feat:兼容上传单个文件的情况

--- prod.config.js
+++ prod.config.js
@@ -1,31 +1,55 @@
 module.exports = {
-  projectName: "xxx", // 项目名称
-  dev: {
-    //采用docker部署的情况
-    name: "106环境",
-    script: "npm run build", // 前端打包命令
-    host: "xxx.xxx.xxx.xxx", // 服务器地址
-    port: 22, // ssh port，一般ssh为22
-    username: "xxxx", // 登录服务器用户名,也可以手动输入
-    password: "xxxxxx", // 登录服务器密码,也可以手动输入
-    distPath: "dist", // 将会将该文件夹打包上传(不可加斜杠)
-    webDir: "/root/temp/messageRouteApp", // 打包文件上传服务器地址(末尾不可加斜杠)
-    // 配置重启命令
-    // restartCmd: "", // 程序上传之后触发重启命令（不配置则不执行,node版本要高于10）,如：pm2 restart webserver --update-env
-    // 下面是关于docke更新的配置
-    dockerName: "messageroute", // docker名称(如果只是上传到服务器，则dockerName和dockerWebDir无需配置)
-    dockerWebDir: "/usr/app/", // 容器中文件存放位置
-  },
-  207: {
-    // 不采用docker部署的情况
-    name: "207环境",
-    script: "npm run build", // 前端打包命令
-    host: "xxx.xxx.xxx.xxx", // 服务器地址
-    port: 22, // ssh port，一般ssh为22
-    username: "xxxx", // 登录服务器用户名
+  projectName: "消息路由APP", // 项目名称
+  script: "npm run build", // 打包脚本
+
+  // 根据需要进行配置，如prod106等（key命名不可出现特殊字符，不可以数字开头），支持多环境部署
+  // 服务器上采用docker方式部署
+  prod106: {
+    name: "prod106环境",
+    host: "192.168.78.106", // 服务器地址,其中xxx为手动输入ip末尾
+    port: 22, // ssh port，一般默认22
+    username: "root", // 登录服务器用户名
     password: "xxxxxx", // 登录服务器密码
-    distPath: "dist", // 将会将该文件夹打包上传,也不加斜杠
-    webDir: "/root/temp/messageRouteApp", // 打包文件上传服务器地址(末尾不要加斜杠)
+    distPath: "dist", // 当前工程中需要上传服务器的文件夹（前不加斜杠标识相对当前工程目录，如加斜杠就需要写出本地完整绝对路径)
+    filter: "*", // 过滤distPath内的文件（只考虑单层文件，不支持递归），只有符合正则的才会上传
+    webDir: "/root/temp/messageRouteApp", // 打包文件上传服务器地址(会将distPath文件夹放过来，如果是nginx做前端配置的话要加distPath)
+    dockerName: "messageroute", // docker名称
+    dockerWebDir: "/usr/app", // 容器中文件存放位置
+  },
+  conf106: {
+    name: "conf106环境",
+    host: "192.168.78.106", // 服务器地址,其中xxx为手动输入ip末尾
+    port: 22, // ssh port，一般默认22
+    username: "root", // 登录服务器用户名
+    password: "xxxxxx", // 登录服务器密码
+    distPath: "vue.config.js", // 当前工程中需要上传服务器的文件名
+    filter: "*", // 过滤distPath内的文件（只考虑单层文件，不支持递归），只有符合正则的才会上传
+    webDir: "/root/temp/messageRouteApp", // 打包文件上传服务器地址(会将distPath文件夹放过来，如果是nginx做前端配置的话要加distPath)
+    // dockerName: 'messageroute', // docker名称
+    // dockerWebDir: '/usr/app' // 容器中文件存放位置
+  },
+  // 服务端直接部署案例
+  104: {
+    name: "104环境",
+    host: "192.168.78.104", // 服务器地址,其中xxx为手动输入ip末尾
+    port: 22, // ssh port，一般默认22
+    username: "root", // 登录服务器用户名
+    password: "xxxxxx", // 登录服务器密码
+    distPath: "dist", // 需要上传服务器的文件夹（前不加斜杠标识相对当前工程目录，如加斜杠就是完整绝对路径)
+    filter: "*", // 过滤distPath内的文件（只考虑单层文件，不支持递归），只有符合正则的才会上传
+    webDir: "/opt/nginx/",
+  },
+  228: {
+    name: "228环境",
+    host: "192.168.78.228", // 服务器地址,其中xxx为手动输入ip末尾
+    port: 22, // ssh port，一般默认22
+    username: "root", // 登录服务器用户名
+    password: "xxxxxx", // 登录服务器密码
+    distPath: "dist", // 需要上传服务器的文件夹（前不加斜杠标识相对当前工程目录，如加斜杠就是完整绝对路径)
+    filter: "*", // 过滤distPath内的文件（只考虑单层文件，不支持递归），只有符合正则的才会上传
+    webDir: "/root/temp/messageRouteApp",
+    dockerName: "messageroute", // docker名称
+    dockerWebDir: "/usr/app", // 容器中文件存放位置
   },
   // 再还有多余的环境按照这个格式写即可
 };
